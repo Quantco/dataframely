fe5a3b679a1fa4137172fb5aba496858e7bd6e7e
gab23r <106454081+gab23r@users.noreply.github.com>
2025-05-15 16:45:58 +0200

    Update tests/test_deprecation.py
    Co-authored-by: Andreas Albert <103571926+AndreasAlbertQC@users.noreply.github.com>

--- tests/test_deprecation.py
+++ tests/test_deprecation.py
@@ -8,16 +8,18 @@ import pytest
 import dataframely as dy
 
 
-def test_integer_constructor_warns_about_nullable() -> None:
-    original_value = os.environ.get("DATAFRAMELY_NO_FUTURE_WARNINGS")
-    os.environ["DATAFRAMELY_NO_FUTURE_WARNINGS"] = "0"
-    try:
-        with pytest.warns(
-            FutureWarning, match="The 'nullable' argument was not explicitly set"
-        ):
-            dy.Integer()
-    finally:
-        if original_value is not None:
-            os.environ["DATAFRAMELY_NO_FUTURE_WARNINGS"] = original_value
-        elif "DATAFRAMELY_NO_FUTURE_WARNINGS" in os.environ:
-            del os.environ["DATAFRAMELY_NO_FUTURE_WARNINGS"]
+def test_column_constructor_warns_about_nullable(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("DATAFRAMELY_NO_FUTURE_WARNINGS", "")
+    with pytest.warns(
+        FutureWarning, match="The 'nullable' argument was not explicitly set"
+    ):
+        dy.Integer()
+        
+@pytest.mark.parametrize("env_var", ["1", "True", "true"]
+def test_future_warning_skip(monkeypatch: pytest.MonkeyPatch, env_var: str) -> None:
+    monkeypatch.setenv("DATAFRAMELY_NO_FUTURE_WARNINGS", env_var)
+    
+    # Elevates FutureWarning to an exception
+    with warnings.catch_warnings():
+        warnings.simplefilter("error", FutureWarning)
+        dy.Integer()
