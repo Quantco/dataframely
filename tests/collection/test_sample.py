fba00ff1c2ac8cd2f319f8a8e8641371339a91a5
Oliver Borchert <oliver.borchert@quantco.com>
2025-08-07 20:31:31 +0200

    feat: Simplify sampling collections with common primary keys

--- tests/collection/test_sample.py
+++ tests/collection/test_sample.py
@@ -1,13 +1,12 @@
 # Copyright (c) QuantCo 2025-2025
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Annotated, Any
+from typing import Annotated
 
 import pytest
 
 import dataframely as dy
 from dataframely.exc import ImplementationError
-from dataframely.random import Generator
 from dataframely.testing.factory import create_collection_raw
 
 
@@ -31,13 +30,6 @@ class MyCollection(dy.Collection):
     first: dy.LazyFrame[MyFirstSchema]
     second: dy.LazyFrame[MySecondSchema] | None
 
-    @classmethod
-    def _preprocess_sample(
-        cls, sample: dict[str, Any], index: int, generator: Generator
-    ) -> dict[str, Any]:
-        sample["a"] = index
-        return sample
-
 
 class MyInlinedCollection(dy.Collection):
     first: Annotated[
@@ -46,13 +38,6 @@ class MyInlinedCollection(dy.Collection):
     ]
     second: dy.LazyFrame[MySecondSchema]
 
-    @classmethod
-    def _preprocess_sample(
-        cls, sample: dict[str, Any], index: int, generator: Generator
-    ) -> dict[str, Any]:
-        sample["a"] = index
-        return sample
-
 
 class MyInlinedCollectionWithOptional(dy.Collection):
     first: Annotated[
@@ -60,13 +45,6 @@ class MyInlinedCollectionWithOptional(dy.Collection):
         dy.CollectionMember(inline_for_sampling=True),
     ]
     second: dy.LazyFrame[MySecondSchema]
-
-    @classmethod
-    def _preprocess_sample(
-        cls, sample: dict[str, Any], index: int, generator: Generator
-    ) -> dict[str, Any]:
-        sample["a"] = index
-        return sample
 
 
 class SmallCollection(dy.Collection):
@@ -79,30 +57,6 @@ class IgnoringCollection(dy.Collection):
         dy.LazyFrame[MySecondSchema], dy.CollectionMember(ignored_in_filters=True)
     ]
 
-    @classmethod
-    def _preprocess_sample(
-        cls, sample: dict[str, Any], index: int, generator: Generator
-    ) -> dict[str, Any]:
-        sample["a"] = index
-        return sample
-
-
-class IncompleteCollection(dy.Collection):
-    first: dy.LazyFrame[MyFirstSchema]
-    second: dy.LazyFrame[MySecondSchema] | None
-
-
-class ErroneousCollection(dy.Collection):
-    first: dy.LazyFrame[MyFirstSchema]
-    second: dy.LazyFrame[MySecondSchema] | None
-
-    @classmethod
-    def _preprocess_sample(
-        cls, sample: dict[str, Any], index: int, generator: Generator
-    ) -> dict[str, Any]:
-        # NOTE: We're NOT assigning the common primary key here
-        return sample
-
 
 # ------------------------------------------------------------------------------------ #
 #                                         TESTS                                        #
@@ -112,7 +66,7 @@ class ErroneousCollection(dy.Collection):
 @pytest.mark.parametrize("n", [0, 1000])
 def test_sample_rows(n: int) -> None:
     collection = MyCollection.sample(n)
-    assert collection.first.collect()["a"].to_list() == list(range(n))
+    assert collection.first.collect().height == n
     assert collection.second is not None
     assert collection.second.collect().is_empty()
 
@@ -124,11 +78,13 @@ def test_sample_with_overrides() -> None:
             {"first": {"b": 8}, "second": [{"c": 6}]},
         ]
     )
-    assert collection.first.collect()["a"].to_list() == [0, 1]
+
+    first_a = collection.first.collect()["a"].to_list()
+    assert len(first_a) == 2
     assert collection.first.collect()["b"].to_list() == [4, 8]
 
     assert collection.second is not None
-    assert collection.second.collect()["a"].to_list() == [0, 0, 1]
+    assert collection.second.collect()["a"].to_list() == [first_a[0]] * 2 + [first_a[1]]
     assert collection.second.collect()["c"].to_list() == [3, 4, 6]
 
 
@@ -146,11 +102,12 @@ def test_sample_inline_with_overrides(
     )
 
     assert collection.first is not None
-    assert collection.first.collect()["a"].to_list() == [0, 1]
+    first_a = collection.first.collect()["a"].to_list()
+    assert len(first_a) == 2
     assert collection.first.collect()["b"].to_list() == [4, 8]
 
     assert collection.second is not None
-    assert collection.second.collect()["a"].to_list() == [0, 0, 1]
+    assert collection.second.collect()["a"].to_list() == [first_a[0]] * 2 + [first_a[1]]
     assert collection.second.collect()["b"].to_list() != [4, 4, 8]
     assert collection.second.collect()["c"].to_list() == [3, 4, 6]
 
@@ -164,22 +121,12 @@ def test_sample_without_dependent_members(n: int) -> None:
 @pytest.mark.parametrize("n", [0, 1000])
 def test_sample_with_ignored_members(n: int) -> None:
     collection = IgnoringCollection.sample(n)
-    assert collection.first.collect()["a"].to_list() == list(range(n))
+    assert collection.first.collect().height == n
 
 
 def test_sample_num_rows_mismatch() -> None:
     with pytest.raises(ValueError, match=r"`num_rows` mismatches"):
         MyCollection.sample(num_rows=1, overrides=[])
-
-
-def test_sample_no_common_primary_key() -> None:
-    with pytest.raises(ValueError, match=r"must contain the common primary keys"):
-        ErroneousCollection.sample()
-
-
-def test_sample_no_overwrite() -> None:
-    with pytest.raises(ValueError, match=r"`_preprocess_sample` must be overwritten"):
-        IncompleteCollection.sample()
 
 
 def test_invalid_inline_for_sampling() -> None:
