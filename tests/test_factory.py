a7a8ff7f62fd5171e5d6f6a3cc869fedbc8d6c16
Moritz Potthoff <moritz.potthoff@quantco.com>
2025-08-14 09:58:01 +0200

    Tests

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,13 +1,20 @@
 # Copyright (c) QuantCo 2025-2025
 # SPDX-License-Identifier: BSD-3-Clause
 
+import polars as pl
 
 import dataframely as dy
+from dataframely._filter import Filter
 from dataframely.testing import create_collection
 
 
 class MySchema(dy.Schema):
     a = dy.Integer(primary_key=True)
+
+
+class MySchema2(dy.Schema):
+    a = dy.Integer(primary_key=True)
+    b = dy.String(nullable=True)
 
 
 class MyCollection(dy.Collection):
@@ -21,17 +28,20 @@ def test_create_collection() -> None:
     # Act
     temp_collection = create_collection(
         "TempCollection",
-        schemas={"member": MySchema, "member2": MySchema},
-        # filters={"testfilter": Filter(lambda c: c.member.filter(pl.col("a") > 0))},
+        schemas={"member": MySchema, "member2": MySchema2},
+        filters={"testfilter": Filter(lambda c: c.member.filter(pl.col("a") > 0))},
     )
 
     # Assert
-    instance = temp_collection.sample(10)
-    assert len(instance.member.collect()) == 10  # type: ignore
-    assert len(instance.member2.collect()) == 10  # type: ignore
-
-    assert not issubclass(temp_collection, MyCollection)
-    assert not hasattr(instance, "foo")
+    instance, _ = temp_collection.filter(
+        {
+            "member": pl.DataFrame({"a": [-1, 1, 2, 3]}),
+            "member2": pl.DataFrame({"a": [-1, 1, 2, 3], "b": ["a", "x", "y", "z"]}),
+        },
+        cast=True,
+    )
+    assert len(instance.member.collect()) == 3  # type: ignore
+    assert len(instance.member2.collect()) == 3  # type: ignore
 
 
 def test_extend_collection() -> None:
@@ -39,14 +49,20 @@ def test_extend_collection() -> None:
     temp_collection = create_collection(
         "TempCollectionExtended",
         collection_base_class=MyCollection,
-        schemas={"member2": MySchema},
-        # additional_filters={"testfilter": Filter(lambda c: c.member.filter(pl.col("a") > 0))},
+        schemas={"member2": MySchema2},
+        filters={"testfilter": Filter(lambda c: c.member.filter(pl.col("a") > 0))},
     )
 
     # Assert
-    instance = temp_collection.sample(10)
-    assert len(instance.member.collect()) == 10  # type: ignore
-    assert len(instance.member2.collect()) == 10  # type: ignore
+    instance, _ = temp_collection.filter(
+        {
+            "member": pl.DataFrame({"a": [-1, 1, 2, 3]}),
+            "member2": pl.DataFrame({"a": [-1, 1, 2, 3], "b": ["a", "x", "y", "z"]}),
+        },
+        cast=True,
+    )
+    assert len(instance.member.collect()) == 3  # type: ignore
+    assert len(instance.member2.collect()) == 3  # type: ignore
 
     assert issubclass(temp_collection, MyCollection)
     assert instance.foo() == "foo"  # type: ignore
