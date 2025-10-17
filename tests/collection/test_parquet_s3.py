612af20bc0ea66b0d6af797271e4bfca01a0a2bc
Oliver Borchert <oliver.borchert@quantco.com>
2025-10-17 15:19:59 +0200

    Update

--- tests/collection/test_parquet_s3.py
+++ tests/collection/test_parquet_s3.py
@@ -1,13 +1,46 @@
 # Copyright (c) QuantCo 2025-2025
 # SPDX-License-Identifier: BSD-3-Clause
 
+import os
+import subprocess
+import uuid
+from collections.abc import Iterator
 
+import boto3
 import pytest
 from polars.testing import assert_frame_equal
 
 import dataframely as dy
 
-pytestmark = pytest.mark.s3
+# ------------------------------------- FIXTURES ------------------------------------- #
+
+
+@pytest.fixture(scope="session")
+def s3_server() -> Iterator[str]:
+    process = subprocess.Popen(["moto_server", "--port", "9999"])
+    yield "http://localhost:9999"
+    process.terminate()
+    process.wait()
+
+
+@pytest.fixture(scope="session")
+def s3_bucket(s3_server: str) -> str:
+    client = boto3.client(
+        "s3", endpoint_url=s3_server, aws_access_key_id="", aws_secret_access_key=""
+    )
+    client.create_bucket(Bucket="test")
+    return "s3://test"
+
+
+@pytest.fixture()
+def s3_tmp_path(s3_server: str, s3_bucket: str, monkeypatch: pytest.MonkeyPatch) -> str:
+    monkeypatch.setenv("AWS_ENDPOINT_URL", s3_server)
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "testing")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "testing")
+    return os.path.join(s3_bucket, str(uuid.uuid4()))
+
+
+# -------------------------------------- SCHEMAS ------------------------------------- #
 
 
 class MyFirstSchema(dy.Schema):
@@ -24,7 +57,7 @@ class MyCollection(dy.Collection):
     second: dy.LazyFrame[MySecondSchema] | None
 
 
-# ------------------------------------------------------------------------------------ #
+# --------------------------------------- TESTS -------------------------------------- #
 
 
 def test_read_write_parquet(s3_tmp_path: str) -> None:
@@ -33,7 +66,6 @@ def test_read_write_parquet(s3_tmp_path: str) -> None:
     collection.write_parquet(s3_tmp_path)
 
     # Act
-    breakpoint()
     read_collection = MyCollection.read_parquet(s3_tmp_path)
 
     # Assert
