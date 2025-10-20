7b4c7b9f9a0b133dc21e672c262b87888335ecc3
Daniel Elsner <daniel.elsner@quantco.com>
2025-10-20 09:04:21 +0200

    Use unique

--- dataframely/functional.py
+++ dataframely/functional.py
@@ -50,18 +50,9 @@ def require_relationship_one_to_one(
         columns, filtered to ensure a 1:1 relationship.
     """
     if filter_unique:
-        return (
-            lhs.group_by(on)
-            .len(LEN_COLUMN)
-            .filter(pl.col(LEN_COLUMN) == 1)
-            .drop(LEN_COLUMN)
-            .join(
-                rhs.group_by(on)
-                .len(LEN_COLUMN)
-                .filter(pl.col(LEN_COLUMN) == 1)
-                .drop(LEN_COLUMN),
-                on=on,
-            )
+        return lhs.unique(on, keep="none").join(
+            rhs.unique(on, keep="none"),
+            on=on,
         )
 
     return lhs.join(rhs, on=on)
@@ -95,13 +86,7 @@ def require_relationship_one_to_at_least_one(
         columns, filtered to ensure a 1:{1,N} relationship.
     """
     if filter_unique:
-        return (
-            lhs.group_by(on)
-            .len(LEN_COLUMN)
-            .filter(pl.col(LEN_COLUMN) == 1)
-            .drop(LEN_COLUMN)
-            .join(rhs.unique(on), on=on)
-        )
+        return lhs.unique(on, keep="none").join(rhs.unique(on), on=on)
 
     return lhs.join(rhs.unique(on), on=on)
 
