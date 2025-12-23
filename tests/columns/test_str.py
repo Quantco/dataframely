127498372d442d913443f1b50d4cee86456afeed
Moritz Potthoff <160181542+MoritzPotthoffQC@users.noreply.github.com>
2025-12-23 11:41:15 +0100

    refactor: By default, sample only simple characters in strings (#252)

--- tests/columns/test_str.py
+++ tests/columns/test_str.py
@@ -1,10 +1,13 @@
 # Copyright (c) QuantCo 2025-2025
 # SPDX-License-Identifier: BSD-3-Clause
+import re
 
 import pytest
 
 import dataframely as dy
 from dataframely.columns import Column
+from dataframely.columns.string import DEFAULT_SAMPLING_REGEX
+from dataframely.random import Generator
 from dataframely.testing import ALL_COLUMN_TYPES
 
 
@@ -32,3 +35,17 @@ def test_string_representation_array() -> None:
 def test_string_representation_struct() -> None:
     column = dy.Struct({"a": dy.String()})
     assert str(column) == dy.Struct.__name__.lower()
+
+
+@pytest.mark.parametrize("min_length", [None, 5, 10])
+@pytest.mark.parametrize("max_length", [None, 20])
+def test_string_sampling_without_regex(
+    min_length: int | None, max_length: int | None
+) -> None:
+    # Check that if no regex is provided, the sampled strings only use
+    # characters from the DEFAULT_SAMPLING_REGEX.
+    column = dy.String(min_length=min_length, max_length=max_length)
+    generator = Generator(seed=42)
+    sample = column.sample(generator=generator, n=1000)
+
+    assert all(re.match(f"{DEFAULT_SAMPLING_REGEX}*", value) for value in sample)
