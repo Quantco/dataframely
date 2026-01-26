54609235a0c92a6c555f654cf5c3d31d88b74eb5
Moritz Potthoff <moritz.potthoff@quantco.com>
2026-01-26 10:01:46 +0100

    fix

--- tests/schema/test_read_write_parquet.py
+++ tests/schema/test_read_write_parquet.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import polars as pl
 import pytest
+from fsspec import url_to_fs
 
 import dataframely as dy
 from dataframely._storage.parquet import SCHEMA_METADATA_KEY
@@ -31,13 +32,20 @@ class MySchema(dy.Schema):
     a = dy.Int64()
 
 
-def test_write_parquet_non_existing_directory(tmp_path: Path) -> None:
+@pytest.mark.parametrize(
+    "any_tmp_path",
+    ["tmp_path", pytest.param("s3_tmp_path", marks=pytest.mark.s3)],
+    indirect=True,
+)
+def test_write_parquet_non_existing_directory(any_tmp_path: str) -> None:
     # Arrange
     df = MySchema.create_empty()
-    file = tmp_path / "non_existing_dir" / "df.parquet"
+    fs = url_to_fs(any_tmp_path)[0]
+    file = fs.sep.join([any_tmp_path, "non_existing_dir", "df.parquet"])
 
     # Act
     MySchema.write_parquet(df, file=file, mkdir=True)
 
     # Assert
-    assert file.exists()
+    result = MySchema.read_parquet(file)
+    assert result.shape == (0, 1)
